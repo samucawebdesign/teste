c2ea75f3b2833e4a747daf2c5e7d41817ca3a3d9
Samuca Webdesign <samuca@samuca.eti.br>
2017-09-08 19:45:02 -0300

    ok

---
+++ dropdown.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+  	<div class="container">
+		<div class="dropdown">
+        	<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Teste <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+            	<li class="dropdown-header">Títulos</li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">PHP</a></li>
+                <li class="divider"></li>
+                <li><a href="#">PHP</a></li>
+            </ul>
+            
+        </div>
+	</div>
+    
+    
+    
+  </body>
+</html>
