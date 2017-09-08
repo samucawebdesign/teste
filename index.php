6036e138bd4846667c099cb8166fb186cd954a51
Samuel de L. Hantschel <samuca@samuca.eti.br>
2017-09-08 18:26:44 -0300

    uppando bootstrap

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+    <div class="container">
+    	<h1>Meu primeiro sistema <small>Bootstrap</small></h1>
+    	<p>Este é meu primeiro teste</p>
+
+    	<div class="row">
+    		<div class="col-sm-3 blue">a</div>
+    		<div class="col-sm-3">a</div>
+    		<div class="col-sm-3">a</div>
+    		<div class="col-sm-3">a</div>
+    	</div>
+
+    	<hr>
+    	<p><mark>Esta é uma frase</mark> específica para o <abbr title="Olha que legal hein">bootstrap</abbr></p>
+    	<hr>
+		<blockquote>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+			<footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+		</blockquote>
+
+    </div>
+
+  </body>
+</html>
