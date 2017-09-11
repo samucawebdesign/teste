169eabb9a86e878bdd1fa081bfe6101435bd0bc0
Samuca Webdesign <samuca@samuca.eti.br>
2017-09-11 18:57:12 -0300

    oo

---
+++ teste.php
@@ -0,0 +1,17 @@
+﻿<?PHP  
+interface Animal
+	{
+		public function andar();
+	}
+
+class Cachorro implements Animal
+	{
+		public function andar()
+			{
+				echo 'Estou andando...';
+			}
+	}
+
+$carhorro = new Cachorro();
+$carhorro->andar();
+?>
