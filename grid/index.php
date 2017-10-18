57b921ff2f7d44caf59d8ad70d27b320450174b6
Samuca Webdesign <samuca@samuca.eti.br>
2017-10-18 11:35:03 -0200

    agora com slideshow

--- grid/index.php
+++ grid/index.php
@@ -56,9 +56,42 @@
   <br>
 
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-12 text-center"><br><br>Slide de imagens<br><br></div>
-		</div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="imagens/certificado.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="imagens/certificado.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="imagens/certificado.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
 	</div>
 
 	<br>
@@ -128,5 +161,10 @@
 			<div class="col-sm-6 text-right fundo2">by Samuca</div>
 		</div>
 	</div>
+    
+    <script type="text/javascript">
+
+	</script>
+    
   </body>
 </html>
